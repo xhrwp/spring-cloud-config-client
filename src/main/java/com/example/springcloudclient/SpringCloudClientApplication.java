aa8ab5ec18b1dc909afab6d21ef674a042413faa
xuhongrong <1qaz2wsx!>
2017-08-16 16:45:49 +0800

    t通过注册中心来实现配置服务

--- src/main/java/com/example/springcloudclient/SpringCloudClientApplication.java
+++ src/main/java/com/example/springcloudclient/SpringCloudClientApplication.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RefreshScope
 @ComponentScan
 @EnableAutoConfiguration
+@EnableDiscoveryClient
 public class SpringCloudClientApplication {
 
 	@Value("${key1}")
